234fe960a04ed5cdda236cd79355d2a7858764b1
SairajGunner <sairaj9396@gmail.com>
2022-11-26 11:56:36 -0500

    Added Welcome Page
    Included animation on the first screen.
    Added the tesseract GIF - Yet to include it on the page.

---
+++ portfolio/src/components/welcome/Welcome.js
@@ -0,0 +1,15 @@
+import './Welcome.scss'
+
+function Welcome() {
+    return (
+        <div>
+            <header className="Welcome-text">
+                <p>Hi there!</p>
+                <p>This is <span className="Welcome-name-animation">SAIRAJ</span></p>
+                <p>Welcome to My Portfolio</p>
+            </header>
+        </div>
+    );
+}
+
+export default Welcome;
