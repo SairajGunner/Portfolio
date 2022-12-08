5da78415949709ca96537a63d6b182068888f513
SairajGunner <sairaj9396@gmail.com>
2022-12-08 11:08:53 -0500

    Changes made to the Header

--- portfolio/src/components/header/Header.js
+++ portfolio/src/components/header/Header.js
@@ -1,8 +1,20 @@
+import './Header.scss';
+
 function Header() {
     return (
         <div>
-            This is the header
-            <hr></hr>
+            <div className="flex-container">
+                <div className="logo">
+                    <img width={"120px"} src="/images/Sairaj - Resize.png"></img>
+                </div>
+                <div className="flex-container-buttons">
+                    <div>Home</div>
+                    <div>Skills</div>
+                    <div>Experience</div>
+                    <div>Education</div>
+                    <div>Contact</div>
+                </div>
+            </div>
         </div>
     );
 }
